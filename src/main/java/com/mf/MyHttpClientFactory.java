7dc80ab8ac17667669cde5f3313db752c3d13ba9
Rishon73 <sshiff@gmail.com>
2018-05-10 13:05:59 -0400

    Adding a class that helps addressing proxy (for accessing the RemoteWebDriver)

---
+++ src/main/java/com/mf/MyHttpClientFactory.java
@@ -0,0 +1,18 @@
+package com.mf;
+
+import java.net.URL;
+
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.openqa.selenium.remote.internal.ApacheHttpClient;
+
+public class MyHttpClientFactory implements org.openqa.selenium.remote.http.HttpClient.Factory {
+    final HttpClientBuilder builder;
+
+    public MyHttpClientFactory(HttpClientBuilder builder) {
+        this.builder = builder;
+    }
+
+    public org.openqa.selenium.remote.http.HttpClient createClient(URL url) {
+        return new ApacheHttpClient(builder.build(), url);
+    }
+}
